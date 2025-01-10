4dfc8b5e97bbe530620ebf2419c9bd8053192356
nmanadan <nmanadan@cps.edu>
2025-01-10 14:23:03 -0600

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+#neil manadan
+#madlib
+#init
+
+#functions
+adjective=input("please enter an adjective")
+clothing=input("please enter an article of clothing")
+location=input("please input a location")
+animal=input("please enter  an animal")
+noun=input("please enter a name")
+verb=input("please enter a verb")
+
+
+#main
+print("welcome to madlibs")
+print("there once was an "+ animal + " at a " + location)
+print("he had a " + adjective + " owner named " + noun)
+print("they "+ verb + " together" )
+print("they wore matching " + clothing)
